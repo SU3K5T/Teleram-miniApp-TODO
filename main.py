6c253ef255d77315659ac9ffcf6eac882ef92f2a
SU3K5T <32840627+SU3K5T@users.noreply.github.com>
2025-01-23 15:55:40 +0000

    added add, completed

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from models import init_db
 import requests as rq
 
-# asyncio
+class AddTask(BaseModel):
+    tg_id: int
+    title: str
+
+class CompleteTask(BaseModel):
+    id: int
 
 @asynccontextmanager
 async def lifespan(app_: FastAPI):
@@ -36,6 +41,17 @@ async def profile(tg_id: int):
     user = await rq.add_user(tg_id)
     completed_tasks_count = await rq.get_completed_tasks_count(user.id)
     return {'completedTasks': completed_tasks_count}
+
+@app.post("/api/add")
+async def add_task(task: AddTask):
+    user = await rq.add_user(task.tg_id)
+    await rq.add_task(user.id, task.title)
+    return {'status': 'ok'}
+
+@app.patch("api/completed")
+async def complete_task(task: CompleteTask):
+    await rq.update_task(task.id)
+    return {'status', 'ok'}
 
 
 
